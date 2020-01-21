d46d1f7a993669209d670fde559cde118b2d43c8
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 14:37:22 +0530

    wayne: Add powerhint.json for sdm660
    Change-Id: Ib3280d38d4bd996c2d4f4ac9c79cec9525879ff4

--- device.mk
+++ device.mk
@@ -304,6 +304,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
