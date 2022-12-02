12eb11c3b0f0483586a6a8fd67f22f5b91b01bca
xiaoleGun <1592501605@qq.com>
2022-12-02 19:46:18 +0800

    wayne: Import qti whitelist
    Change-Id: I2b2421a80654e4f8e876a6dc78001dcbe6c3a3e3
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -213,6 +213,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# Low power Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0.vendor
