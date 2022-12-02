65baef931344041b36923bd22bd00b438f29e744
xiaoleGun <1592501605@qq.com>
2022-12-02 19:07:44 +0800

    wayne: Build common xiaomi lights AIDL
    Change-Id: Ia529f93048f9e9a69cea804dda11c5a7312066dd
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -192,6 +192,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0.vendor
